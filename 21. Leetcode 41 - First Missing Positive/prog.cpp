0b57887847390b78b96606f09b16de3e51fad891
Avadhesh kumar shah <avadheshkumarshah578@gmail.com>
2025-07-07 19:10:55 +0530

    Leetcode - 41

--- 21. Leetcode 41 - First Missing Positive/prog.cpp
+++ 21. Leetcode 41 - First Missing Positive/prog.cpp
@@ -13,3 +13,41 @@ public:
         return res;
     }
 };
+
+
+// 2nd mathode using set
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int i=0;i<nums.size();i++){
+            st.insert(nums[i]);
+        }
+        for(int i=1;i<=st.size()+1;i++){
+            if(st.find(i)==st.end()) return i;
+        }
+        return -1;
+    }
+};
+
+
+// 3rd methode using cyclic sort
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();        
+        for(int i=0;i<n;i++){   
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
+                swap(nums[i], nums[nums[i]-1]); 
+            };
+        }
+
+        // we check for the first missing positive integer
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1; 
+        }
+        return n+1; // if all numbers are in their correct position, return n+1
+
+    }
+};
