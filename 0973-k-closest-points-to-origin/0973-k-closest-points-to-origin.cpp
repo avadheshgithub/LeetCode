196a2a9c7bad9338385e4495226669bcf8155827
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2025-12-31 12:17:25 +0530

    Time: 102 ms (21.61%), Space: 82.1 MB (22.25%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
+    struct comp {
+        bool operator()(const vector<int>& a, const vector<int>& b) {
+            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
+        }
+    };
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1]*b[1];
-        });
-        
-        points.resize(k);          
-        return points;
+        priority_queue<vector<int>, vector<vector<int>>, comp> pq;//max heap
+
+        for(auto point:points){
+            pq.push(point);
+            if(pq.size()>k) pq.pop();
+        }
+
+        vector<vector<int>>ans;
+        for(int i=0;i<k;i++){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+
     }
 };
