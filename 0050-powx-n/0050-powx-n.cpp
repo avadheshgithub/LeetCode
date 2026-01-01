8305b3dcabf933db2524f5fab661a0af78477f40
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-01 18:38:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.44%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1.0;
+        double current = x;
+        
+        for (long long i=N; i>0; i/=2) {
+            if(i%2==1) result *= current;   //odd
+            current *= current;     //even
+        }
+        return result;
+    }
+};
