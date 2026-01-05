b76271abe9df1ecaaa2b3feae655fb595b6932c9
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-05 22:06:14 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.62%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,9 @@ public:
     int hammingWeight(int n) {
         int count = 0;
 
-        while(n!=0){
-            count++;
-            n = n & (n-1);
+        for(int i=0;i<32;i++){
+            if((n&1)==1)count++;
+            n = n>>1;
         }
         return count;
     }
