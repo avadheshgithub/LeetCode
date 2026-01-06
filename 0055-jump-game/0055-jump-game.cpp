0a1a1ca0c55a7027995310757fdada082ed100cb
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-07 00:28:59 +0530

    Time: 0 ms (100%), Space: 52.3 MB (35.32%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int final = n-1;
+
+        for(int i=n-2;i>=0;i--){
+            if(i+nums[i]>=final){
+                final = i;
+            }
+        }
+        return final==0;
+    }
+};
