9df8a067c579fe64983c7ff26f68291b65c1c17d
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-07 09:56:10 +0530

    Time: 0 ms (100%), Space: 20.5 MB (61.25%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int count=0,currMax=0,currEnd=0;
+
+        for(int i=0;i<nums.size()-1;i++){
+            currMax = max(currMax,i+nums[i]);
+            if(i==currEnd){
+                count++;
+                currEnd = currMax;
+            }
+        }
+        return count;
+    }
+};
