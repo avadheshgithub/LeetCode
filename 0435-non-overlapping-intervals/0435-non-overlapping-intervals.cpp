f1b93043be72531f5aeab4dfe7c3df611f9bdf13
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-09 00:33:42 +0530

    Time: 59 ms (33.37%), Space: 94 MB (36.15%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,22 +1,22 @@
-bool cmp(vector<int>&a,vector<int>&b){
-    return a[1] < b[1]; // sorting based on the end time
-}
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& A) {
-        
-        sort(A.begin(),A.end(),cmp);
-        int count = 0;
-        int lastEndTime = A[0][1];
+    int eraseOverlapIntervals(vector<vector<int>>& Arr) {
+       sort(Arr.begin(),Arr.end());
 
-        for(int i=1;i<A.size();i++){
-            if(A[i][0] < lastEndTime){
+       int prev = 0;
+       int count = 0;
+
+       for(int i=1;i<Arr.size();i++){
+            if(Arr[prev][1]>Arr[i][0]){
+                if(Arr[prev][1]>Arr[i][1]){
+                    prev = i;
+                }
                 count++;
             }
             else{
-                lastEndTime = A[i][1];
+                prev = i;
             }
-        }
-        return count;
+        } 
+       return count;
     }
 };
