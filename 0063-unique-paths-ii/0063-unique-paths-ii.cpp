bd2e38ebc9cece567225f64daa31760cce84883d
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-11 12:37:02 +0530

    Time: 0 ms (100%), Space: 12 MB (27.78%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+        int m = arr.size(), n = arr[0].size();
+        if (arr[0][0]==1 or arr[m-1][n-1]==1) return 0;
+
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]=1;    
+
+        for (int i = 1; i < m; ++i) { // fill first col
+            if (arr[i][0] == 0) 
+                dp[i][0] = dp[i-1][0];
+        }
+        
+        for (int j = 1; j < n; ++j) { //fill first row
+            if (arr[0][j] == 0) 
+                dp[0][j] = dp[0][j-1];
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(arr[i][j]==1) dp[i][j]= 0;
+                else dp[i][j]= dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
