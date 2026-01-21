6904e70aba22fa08012ec804b5ed32c36bf1c80d
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-21 12:32:03 +0530

    Time: 0 ms (100%), Space: 15.2 MB (48.77%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void order(TreeNode* root, int level) {
-        if(root == NULL) return;
-
-        if(ans.size() == level) {
-            ans.push_back(vector<int>());
-        }
-
-        ans[level].push_back(root->val);
-
-        order(root->left, level + 1);
-        order(root->right, level + 1);
-    }
-
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        ans.clear();
-        order(root, 0);
+        if(root==NULL) return {};
 
-        // reverse odd level
-        for(int i=1; i<ans.size(); i+=2) {
-            reverse(ans[i].begin(),ans[i].end());
+        queue<TreeNode*>q;
+        q.push(root);
+
+        bool level = true;
+        vector<vector<int>>ans;
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>temp(size);
+
+            for(int i=0;i<size;i++){
+                TreeNode *node = q.front();
+                q.pop();
+
+                int index = level ? i: size-i-1; // odd/even
+                temp[index] = node->val;
+
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            ans.push_back(temp);
+            level = !level; // odd/Even
         }
-
         return ans;
     }
 };
