f0fd90640778a7dc78c5eeb885b272880725f42e
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-21 23:08:36 +0530

    Time: 0 ms (100%), Space: 22.1 MB (8.63%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void inorder(TreeNode* root,vector<int>&ans){ 
         if(root==NULL) return;
+        
         inorder(root->left,ans);
         ans.push_back(root->val);
         inorder(root->right,ans);
