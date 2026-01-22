d4bb40edde57026b30e3815a8734d0ad5596a1c8
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-22 18:36:11 +0530

    Time: 0 ms (100%), Space: 25.9 MB (10.87%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    void inOrder(TreeNode* root,vector<int>&ans){
+        if(root==NULL) return;
+
+        inOrder(root->left,ans);
+        ans.push_back(root->val);
+        inOrder(root->right,ans);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        vector<int>ans;
+        inOrder(root,ans);
+
+        int dif = INT_MAX;
+        for(int i=1;i<ans.size();i++){
+            dif = min(dif,ans[i]-ans[i-1]);
+        }
+        return dif;
+    }
+};
