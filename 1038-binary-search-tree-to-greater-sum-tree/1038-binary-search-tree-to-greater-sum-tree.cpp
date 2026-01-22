8bc6fa3c48a201c7d10de0ec2a1255d84f782abe
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-01-22 19:35:08 +0530

    Time: 0 ms (100%), Space: 11.4 MB (47.42%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sum = 0;
+    TreeNode* revInOrder(TreeNode* root){
+        if(root==NULL) return 0;
+
+        revInOrder(root->right);
+        root->val += sum;
+        sum = root->val;
+        revInOrder(root->left);
+
+        return root;
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root==NULL) return NULL;
+        return revInOrder(root);
+    }
+};
