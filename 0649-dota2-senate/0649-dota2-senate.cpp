06df890ec370c6b990ce094d5cecd6e56d7f8ba4
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-03 14:41:37 +0530

    Time: 2 ms (73.77%), Space: 11.6 MB (51.56%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     string predictPartyVictory(string senate) {
         int n = senate.length();
-        queue<int>R,D;
+
+        queue<int>R;
+        queue<int>D;
 
         for(int i=0;i<n;i++){
             if(senate[i]=='R') R.push(i);
@@ -12,15 +14,14 @@ public:
         while(!R.empty() && !D.empty()){
             int Ri = R.front();
             int Di = D.front();
-            if(Ri<Di){
-                R.push(Ri+n);
-            }
-            else {
-                D.push(Di+n);
-            }
+
+            if(Ri<Di) R.push(Ri+n);
+            if(Ri>Di) D.push(Di+n);
+
             R.pop();
             D.pop();
         }
-        return R.empty()? "Dire" : "Radiant";
+
+        return R.empty() ? "Dire" : "Radiant";
     }
 };
