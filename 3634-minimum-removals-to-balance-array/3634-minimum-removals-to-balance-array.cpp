2a6a68a52610eb74299afdf330cbb944148c64ad
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-06 22:56:26 +0530

    Time: 26 ms (89.24%), Space: 105 MB (52.91%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -5,11 +5,11 @@ public:
         sort(nums.begin(), nums.end());
 
         int ans = n, R = 0;
-        for (int L = 0; L < n; ++L) {
-            while (R < n && nums[R] <= static_cast<long long>(nums[L]) * k) {
+        for (int L=0;L<n; ++L) {
+            while (R<n && nums[R] <= static_cast<long long>(nums[L]) * k) {
                 ++R;
             }
-            ans = min(ans, n - (R - L));
+            ans = min(ans, n-(R-L));
         }
         return ans;
     }
