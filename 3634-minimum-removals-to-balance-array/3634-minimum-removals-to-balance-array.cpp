12e2d7c8d4862f2306dc58f44d59674596a39b13
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-06 22:55:49 +0530

    Time: 40 ms (29.36%), Space: 105 MB (52.91%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k ) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        int ans = n, R = 0;
+        for (int L = 0; L < n; ++L) {
+            while (R < n && nums[R] <= static_cast<long long>(nums[L]) * k) {
+                ++R;
+            }
+            ans = min(ans, n - (R - L));
+        }
+        return ans;
+    }
+};
