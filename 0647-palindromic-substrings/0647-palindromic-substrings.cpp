362941884a188512ad0768e232e0bb5563b6d78e
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-11 00:50:28 +0530

    Time: 10 ms (56.95%), Space: 21.2 MB (24.2%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int checkPalindrome(string s,int L, int R){
+        int count=0;
+        while(L>=0 && R<=s.length() && s[L]==s[R]){
+            count++;
+            L--;
+            R++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            ans += checkPalindrome(s,i,i);  // odd
+            ans += checkPalindrome(s,i,i+1); //even
+        }
+        return ans;
+    }
+};
