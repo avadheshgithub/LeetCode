c06814e7caa01d6053c6b441d7436d94c18cfcb0
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-11 01:14:56 +0530

    Time: 7 ms (32.71%), Space: 15.7 MB (38.33%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        string temp = s + "#" + rev;
+        int n = temp.size();
+
+        vector<int> lps(n, 0);
+
+        for(int i = 1; i < n; i++){
+            int j = lps[i - 1];
+            while(j > 0 && temp[i] != temp[j])
+                j = lps[j - 1];
+            if(temp[i] == temp[j])
+                j++;
+            lps[i] = j;
+        }
+
+        int palLen = lps[n - 1];   // longest palindromic prefix
+        string suffix = s.substr(palLen);
+        reverse(suffix.begin(), suffix.end());
+
+        return suffix + s;
+    }
+};
