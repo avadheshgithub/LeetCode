8930dd53680ff87ece6abef251a92e011f2ea235
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-10 10:59:50 +0530

    Time: 2 ms (71.07%), Space: 19.1 MB (15.45%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -2,20 +2,21 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>> PQ;
-        for(ListNode* list:lists){
-            while(list!=NULL){
-                PQ.push(list->val);
-                list = list->next;
+        priority_queue<int,vector<int>,greater<int>>pq;
+
+        for(auto x:lists){
+            while(x!=NULL){
+                pq.push(x->val);
+                x = x->next;
             }
         }
-        ListNode* dummy=new ListNode(-1);
-        ListNode* merge=dummy;
 
-        while(!PQ.empty()){
-            merge->next = new ListNode(PQ.top());
-            PQ.pop();
-            merge = merge->next;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while(!pq.empty()){
+          temp->next = new ListNode(pq.top());
+          pq.pop();
+          temp = temp->next;   
         }
         return dummy->next;
     }
