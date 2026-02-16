a1c4511aa33474d557d0729d3983a2076743f3b9
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-16 12:53:55 +0530

    Time: 3 ms (77.34%), Space: 19.2 MB (81.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    bool isCycleDFS(int src,vector<bool>&vis,vector<bool>&recPath,vector<vector<int>>&edges){
-        vis[src]= true;
-        recPath[src]= true;
+    bool isCycleDFS(int src, vector<bool>& vis,vector<bool>& recPath,vector<vector<int>>& adj) {
 
-        for(int i=0;i<edges.size();i++){ 
-            int v = edges[i][0];  // [1 V,0 U] --> (0->1) U-->V
-            int u = edges[i][1];
+        vis[src] = true;
+        recPath[src] = true;
 
-            if(u==src){
-                if(!vis[v]){
-                    if(isCycleDFS(v,vis,recPath,edges)) return true;
-                }
-                else if(recPath[v]) return true;
+        for (int v : adj[src]) { 
+            if (!vis[v]) {
+                if (isCycleDFS(v, vis, recPath, adj)) return true;
+            }
+            else if (recPath[v]) { // backEdge --> Cycle
+                return true;
             }
         }
         recPath[src] = false;
         return false;
     }
-    bool canFinish(int n, vector<vector<int>>& edges) {
-        vector<bool>vis(n,false);
-        vector<bool>recPath(n,false);
 
-        for(int i=0;i<n;i++){  //--> for disconnected Graph
-            if(!vis[i]){
-                if(isCycleDFS(i,vis,recPath,edges)) return false;
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);     // adjacency List
+        for (auto &e : edges) {
+            adj[e[1]].push_back(e[0]);   // u → v
+        }
+
+        vector<bool> vis(n, false);
+        vector<bool> recPath(n, false);
+
+        for(int i=0; i<n; i++) { // Cycle Detection
+            if (!vis[i]) {
+                if (isCycleDFS(i, vis, recPath, adj)) return false;
             }
         }
         return true;
