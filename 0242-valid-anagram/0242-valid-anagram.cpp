f8ae1318358856a81279afb7a69ada1390e40bdf
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-22 10:02:58 +0530

    Time: 7 ms (19.63%), Space: 9.8 MB (24.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        map<char,int>mp;
+        for(auto x:s) mp[x]++;
+        for(auto x:t) mp[x]--;
+        for(auto x:mp){
+            if(x.second != 0) return false;
+        }
+        return true;
+    }
+};
