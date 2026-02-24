86ecf97c263aa74b80c046eb49bd19c3b2f80836
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-02-24 09:44:21 +0530

    Time: 0 ms (100%), Space: 17.3 MB (75.77%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int inOrder(TreeNode* root,int s){
+        if (root==NULL) return 0;
+
+        s = (s << 1)|root->val; // binary->Num
+
+        if (root->left == NULL && root->right == NULL) return s;
+
+        return inOrder(root->left, s) +  inOrder(root->right, s);
+    }
+
+    int sumRootToLeaf(TreeNode* root) {
+        return inOrder(root, 0);
+    }
+};
