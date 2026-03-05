c7fa05f2cb7de6659515a80aa14ff7cb7877dcbe
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-03-05 23:04:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (76.2%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int minOperations(string s) {
         int cnt = 0, n = s.size();
-        for (int i = 0; i < n; i++) cnt += s[i] != "01"[i & 1];
+        for (int i = 0; i < n; i++) 
+            cnt += s[i] != "01"[i & 1];
         return min(cnt, n-cnt);
     }
 };
