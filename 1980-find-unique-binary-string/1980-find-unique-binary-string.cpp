bd3f3cabdb51cdf80b259dcf77041a44a2f07467
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-03-08 23:41:29 +0530

    Time: 0 ms (100%), Space: 13.3 MB (24.02%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        unordered_set<string> strSet(nums.begin(), nums.end());
+        string cur(nums.size(), '0');
+        return backtrack(0, cur, strSet, nums.size());
+    }
+
+private:
+    string backtrack(int i, string& cur, unordered_set<string>& strSet, int n) {
+        if (i == n) {
+            return strSet.count(cur) ? "" : cur;
+        }
+
+        string res = backtrack(i + 1, cur, strSet, n);
+        if (!res.empty()) return res;
+
+        cur[i] = '1';
+        return backtrack(i + 1, cur, strSet, n);
+    }
+};
