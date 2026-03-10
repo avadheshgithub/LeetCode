a096b53cacb39f0428015bb01442d82b37ac0bf2
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-03-10 19:58:20 +0530

    Time: 27 ms (3.23%), Space: 21.5 MB (6.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,6 +4,7 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         vector<vector<int>> visited = matrix;
+        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
