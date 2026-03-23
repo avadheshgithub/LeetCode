6826e04cd768ff2e4eb7af580733b5caec094e90
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-03-23 19:35:02 +0530

    Time: 2 ms (41.03%), Space: 14.1 MB (54.26%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -11,12 +11,11 @@ public:
     
     maxDp[0][0] = minDp[0][0] = grid[0][0];
     
-    // First column
     for(int i = 1; i < m; i++) {
         maxDp[i][0] = minDp[i][0] = maxDp[i-1][0] * grid[i][0];
     }
     
-    // First row
+
     for(int j = 1; j < n; j++) {
         maxDp[0][j] = minDp[0][j] = maxDp[0][j-1] * grid[0][j];
     }
