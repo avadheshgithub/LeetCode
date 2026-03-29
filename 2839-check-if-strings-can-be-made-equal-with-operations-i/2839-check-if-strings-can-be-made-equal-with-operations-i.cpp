17c9a545fe787ff54fad50c7fbf0de42a031e84a
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-03-29 12:00:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (32.9%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        return (
-        multiset<char>{s1[0], s1[2]} == multiset<char>{s2[0], s2[2]} &&
-        multiset<char>{s1[1], s1[3]} == multiset<char>{s2[1], s2[3]}
-        ); 
+        vector<char> a = {s1[0], s1[2]};
+        vector<char> b = {s1[1], s1[3]};
+        vector<char> c = {s2[0], s2[2]};
+        vector<char> d = {s2[1], s2[3]};
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        sort(c.begin(), c.end());
+        sort(d.begin(), d.end());
+
+        return (a == c && b == d);
     }
 };
