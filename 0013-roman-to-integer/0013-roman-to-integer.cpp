ab2447729008a1a1e829182a06f3190f0589380f
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-04-03 20:42:36 +0530

    Time: 14 ms (11.89%), Space: 13.5 MB (18.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.size();
         unordered_map<char, int> roman = {
             {'I', 1},
             {'V', 5},
@@ -11,9 +10,9 @@ public:
             {'D', 500},
             {'M', 1000}
         };
-        int result=0;
-        for(int i=0;i<n;i++){
-            if(i+1<n && roman[s[i]] < roman[s[i+1]]){
+        int result = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (i+1 < s.length() && roman[s[i]] < roman[s[i+1]]) {
                 result -= roman[s[i]]; // XL --> X>L --> L-X
             } 
             else result += roman[s[i]];// LX --> X<L --> L+X
