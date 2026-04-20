0db4600e6bdab53279868122b4d862300b3c8294
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-04-20 09:30:36 +0530

    Time: 3 ms (45.7%), Space: 10.8 MB (13.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,9 +3,11 @@ public:
     bool isPalindrome(int n) {
         string str = to_string(n);
         int len = str.length();
+
         for(int i=0;i<len/2;i++){
             if(str[i]!=str[len-i-1]) return false;
         }
+        
         return true;
     }
 };
