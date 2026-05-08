dc4e9da1d38032dc2bddf69ee7fab7979bb1ac3e
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-05-08 22:56:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.43%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -6,10 +6,10 @@ public:
         
         while(l<=r){
             long long m = (l+r)/2;
-            long long m_sqaure = m*m;
+            long long m_sqaq = m*m;
 
-            if(m_sqaure==x) return m;
-            else if(m_sqaure<x) l = m+1;
+            if(m_sqaq==x) return m;
+            else if(m_sqaq<x) l = m+1;
             else r = m-1;
         }
         return r;
