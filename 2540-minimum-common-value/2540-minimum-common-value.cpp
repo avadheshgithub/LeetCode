9b049f9e302d540ddd1f9a3c7141fff3f5637036
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-05-19 09:27:44 +0530

    Time: 0 ms (100%), Space: 54.6 MB (58.81%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getCommon(vector<int>& A, vector<int>& B) {
         int i = 0, j = 0;
-
+        
         while (i<A.size() && j<B.size()) {
             if (A[i] == B[j])  return A[i];
             else if (A[i] < B[j]) i++;
