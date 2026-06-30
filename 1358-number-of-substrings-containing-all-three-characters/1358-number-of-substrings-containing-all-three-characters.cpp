c8f9d57f42f5ad03a44343c748c4d9303860d517
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-06-30 09:33:09 +0530

    Time: 4 ms (79.36%), Space: 11.2 MB (33.33%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -4,6 +4,7 @@ public:
         int ans = 0;
         vector<int>freq(3,0);
         int j = 0;
+        
         for(int i=0;i<s.size();i++){
             freq[s[i]-'a']++;
             while(freq[0]>0 && freq[1]>0 && freq[2]>0){
