f2029ac2b9f64282e8483494f5393acd491f4e28
Avadhesh kumar shah <141864136+avadheshgithub@users.noreply.github.com>
2026-08-18 08:02:39 +0530

    Time: 0 ms (100%), Space: 29.1 MB (78.95%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count[51] = {0};
+
+        // Check every subarray of size k
+        for (int i = 0; i <= n - k; i++) {
+            bool present[51] = {false};
+
+            for (int j = i; j < i + k; j++) {
+                present[nums[j]] = true;
+            }
+
+            // Count each number only once per subarray
+            for (int x = 0; x <= 50; x++) {
+                if (present[x]) {
+                    count[x]++;
+                }
+            }
+        }
+
+        // Find largest number appearing in exactly one subarray
+        for (int x = 50; x >= 0; x--) {
+            if (count[x] == 1) {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+};
